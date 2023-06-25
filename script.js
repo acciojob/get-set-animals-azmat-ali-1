f4457f0847205fb60d54503eadace375f11241e7
Azmat-Ali-1 <114492086+azmat-ali-1@users.noreply.github.com>
2023-06-25 18:16:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 //complete this code
-class Animal {}
+class Person {
+  constructor(name, age) {
+    this._name = name;
+    this._age = age;
+  }
 
-class Dog extends Animal {}
+  get name() {
+    return this._name;
+  }
 
-class Cat extends Animal {}
+  set age(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this._name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(`${this._name} is teaching.`);
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
